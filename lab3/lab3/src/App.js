f4e2d1420fcb4e26a8e3781fe96618494201761e
Kevin Connolly <46018813+dailykcon@users.noreply.github.com>
2021-02-06 16:12:15 -0500

    Lab 3 Complete

---
+++ lab3/lab3/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css';
+import HomePage from "./components/Homepage";
+
+function App() {
+  return (
+    <HomePage firstName={'Kevin'}/>
+  );
+}
+
+export default App;
---
+++ lab3/lab3/src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+
+    render() {
+
+        render() 
+            return(
+              <h1>{this.props.firstName}</h1>
+            );
+          }
+    }
+
+export default HomePage;
